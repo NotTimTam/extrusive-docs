b4ff07aa262debee5583c89ed069da64f7310825
NotTimTam <timothyrp6@gmail.com>
2023-05-10 18:08:27 -0700

    Added script.

--- extrusive-docs/file-tree.js
+++
@@ -1,1 +0,0 @@
-const fileTree = {"path":"/content","name":"content","children":[{"path":"/content/Get Started","name":"Get Started","children":[{"path":"/content/Get Started/QUICK START.html","name":"QUICK START.html"}]},{"path":"/content/README.html","name":"README.html"}]}
